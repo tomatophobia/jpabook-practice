7e8eb2a65f639145aa3ec140e337f81b0b3fead1
USER <user@AL02466422.local>
2024-03-20 22:56:52 +0900

    자바로 변경해서 테스트

---
+++ src/main/kotlin/jpabook/start/pr4/Team.java
@@ -0,0 +1,50 @@
+package jpabook.start.pr4;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ * @author 최영서 (youngseo.choi@navercorp.com)
+ */
+@Entity
+@Table(name = "team")
+public class Team {
+	@Id
+	@Column(name = "team_id")
+	private String id;
+
+	private String name;
+
+	@OneToMany(mappedBy = "team", fetch = FetchType.LAZY)
+	private List<Member> members;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Member> getMembers() {
+		return members;
+	}
+
+	public void setMembers(List<Member> members) {
+		this.members = members;
+	}
+}
